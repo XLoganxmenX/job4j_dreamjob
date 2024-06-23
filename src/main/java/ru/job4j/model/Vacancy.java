6865b2870c8907ba822448e38af2dd4a1c82ecdd
loganxmen <loganxmen97@gmail.com>
2024-06-23 11:23:26 +0800

    add Vacancy model, VacancyRepository, realization of vacancies list  [#504841]

---
+++ src/main/java/ru/job4j/model/Vacancy.java
@@ -0,0 +1,69 @@
+package ru.job4j.model;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Objects;
+
+public class Vacancy {
+
+    private int id;
+    private String title;
+    private String description;
+    private LocalDateTime creationDate;
+
+    public Vacancy(int id, String title, String description) {
+        this.id = id;
+        this.title = title;
+        this.description = description;
+        this.creationDate = LocalDateTime.now();
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getCreationDate() {
+        return creationDate.format(DateTimeFormatter.ofPattern("dd-MM-yyyy HH:mm:ss"));
+    }
+
+    public void setCreationDate(LocalDateTime creationDate) {
+        this.creationDate = creationDate;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        Vacancy vacancy = (Vacancy) o;
+        return id == vacancy.id;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+}
---
+++ src/main/java/ru/job4j/repository/MemoryVacancyRepository.java
@@ -0,0 +1,62 @@
+package ru.job4j.repository;
+
+import ru.job4j.model.Vacancy;
+
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Optional;
+
+public class MemoryVacancyRepository implements VacancyRepository {
+
+    private static final MemoryVacancyRepository INSTANCE = new MemoryVacancyRepository();
+
+    private int nextId = 1;
+
+    private final Map<Integer, Vacancy> vacancies = new HashMap<>();
+
+    private MemoryVacancyRepository() {
+        save(new Vacancy(0, "Intern Java Developer", "70 000 rub."));
+        save(new Vacancy(0, "Junior Java Developer", "90 000 rub."));
+        save(new Vacancy(0, "Junior+ Java Developer", "110 000 rub."));
+        save(new Vacancy(0, "Middle Java Developer", "150 000 rub."));
+        save(new Vacancy(0, "Middle+ Java Developer", "200 000 rub."));
+        save(new Vacancy(0, "Senior Java Developer", "250 000 rub."));
+    }
+
+    public static MemoryVacancyRepository getInstance() {
+        return INSTANCE;
+    }
+
+    @Override
+    public Vacancy save(Vacancy vacancy) {
+        vacancy.setId(nextId++);
+        vacancies.put(vacancy.getId(), vacancy);
+        return vacancy;
+    }
+
+    @Override
+    public void deleteById(int id) {
+        vacancies.remove(id);
+    }
+
+    @Override
+    public boolean update(Vacancy vacancy) {
+        return vacancies.computeIfPresent(vacancy.getId(),
+                (id, oldVacancy) -> new Vacancy(
+                        oldVacancy.getId(),
+                        vacancy.getTitle(),
+                        vacancy.getDescription())
+                ) != null;
+    }
+
+    @Override
+    public Optional<Vacancy> findById(int id) {
+        return Optional.ofNullable(vacancies.get(id));
+    }
+
+    @Override
+    public Collection<Vacancy> findAll() {
+        return vacancies.values();
+    }
+}
